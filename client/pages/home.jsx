ccd70b5866ee6df9b5e9ec3005b1b35d5de1ff23
Frank-Develops <167141334+Frank-Develops@users.noreply.github.com>
2024-08-02 15:10:19 -0500

    finished

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -28,12 +28,12 @@ export default class Home extends React.Component {
           <NetworkError tryAgain={this.props.tryAgain} toggleCalling={this.props.toggleCalling} />
         }
         <div className="image-holder-row">
-          <img src="/images/683813.jpg" alt="The Mandalorian" id={38963} onClick={this.showInfo} />
-          <img src="/images/746929.jpg" alt="Superman and Lois" id={44751} onClick={this.showInfo} />
+          <img src="/images/1161209.jpg" alt="Strange New Worlds" id={48090} onClick={this.showInfo} />
+          <img src="/images/1323819.jpg" alt="Batman: Caped Crusader" id={55415} onClick={this.showInfo} />
         </div>
         <div className="image-holder-row">
-          <img src="/images/739854.jpg" alt="The Falcon and The Winter Soldier" id={41749} onClick={this.showInfo} />
-          <img src="/images/679902.jpg" alt="The Simpsons" id={83} onClick={this.showInfo} />
+          <img src="/images/728576.jpg" alt="Chilling Adventures of Sabrina" id={32649} onClick={this.showInfo} />
+          <img src="/images/1321857.jpg" alt="Emily in Paris" id={41632} onClick={this.showInfo} />
         </div>
       </main>
       <footer onClick={this.props.closeMenu}>
